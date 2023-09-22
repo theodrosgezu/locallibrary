86c711c34d7bd013df1907ad09ad0091cff239f9
theodrosgezu <teddyegezu@gmail.com>
2023-09-22 11:46:13 +0300

    creating the homepage

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count, Q
 from django.shortcuts import render
 
 # Create your views here.
@@ -17,11 +18,19 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Count of books that contain a particular word
+    word = 'ted'  # replace with your word
+    num_books_word = Book.objects.filter(Q(title__icontains=word) | Q(summary__icontains=word)).count()
+
+    # Count of genres that contain a particular word
+    num_genres_word = Genre.objects.filter(name__icontains=word).count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
-        'num_authors': num_authors,
+        'num_authors': num_authors,'num_books_word': num_books_word,
+        'num_genres_word': num_genres_word,
     }
 
     # Render the HTML template index.html with the data in the context variable
--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -124,3 +124,19 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = ['*']
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
